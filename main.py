a0cd1fcbf7ccae4cb96f751621a6c270ac12df96
Philip Sanchez <philipsanchez14@csu.fullerton.edu>
2026-03-27 12:43:47 -0700

    Started on creating the Tetris UI, currrently just displaying a 10x20 grid

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import pygame
+from game import Grid
+import sys
+
+pygame.init()
+dark_blue = (44, 44, 127)
+
+screen = pygame.display.set_mode((300,600))
+pygame.display.set_caption("Tetris")
+
+clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+while True :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            pygame.quit()
+            sys.exit()
+    
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
+
