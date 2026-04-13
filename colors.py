ec3445bb49fc3cd47b57091fdb7744b53398734c
JoshuaH05 <joshuahigareda@fullerton.edu>
2026-04-12 19:43:53 -0700

    Worked on UI Visuals. Just Score and Next Block Display left for UI

--- colors.py
+++ colors.py
@@ -5,9 +5,12 @@ class Colors:
     orange = (226,116,17)
     yellow = (237,234,4)
     purple = (166, 0, 247)
-    light_blue = (21,204,209)
+    cyan = (21,204,209)
     blue = (13,64,216)
+    white = (255, 255, 255)
+    dark_blue = (44, 44, 127)
+    light_blue = (59, 85, 162)
 
     @classmethod
     def get_cell_colors(cls) :
-        return [cls.grey, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.light_blue, cls.blue]
+        return [cls.grey, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.cyan, cls.blue]
